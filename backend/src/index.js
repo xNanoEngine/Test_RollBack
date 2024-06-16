db67ae82a2792ed0241e1c0c12538f9743978684
xNanoEngine <xnanoengine@gmail.com>
2024-06-15 20:55:01 -0400

    error genrated

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,10 +5,12 @@ const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.send("¡Hola Mundo!");
 });
+aaa;
 
 app.get("/error", (req, res) => {
   res.send("¡Error resuelto!");
 });
+app;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
