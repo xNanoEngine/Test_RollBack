d2a8affe525bc826c6fbac12f2e0e6be2bb0b64b
xNanoEngine <xnanoengine@gmail.com>
2024-06-15 20:37:09 -0400

    error prueba

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,11 @@ const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.send("¡Hola Mundo!");
 });
-
+app.get("/error", (req, res) => {
+  res;
+  aaa;
+  res.send("¡Error resuelto!");
+});
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
